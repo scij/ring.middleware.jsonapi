08b4701501ae24a70ef9a3ad79f983023c6f4ec0
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2019-01-24 13:47:12 +0100

    Add project.clj #1

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject ring.middleware.jsonapi "0.0.0"
+  :description "A simple Ring's middleware that generates JSON API response"
+  :url "https://github.com/druids/ring.middleware.jsonapi"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[metosin/muuntaja "0.6.3"]]
+
+  :cloverage
+  {:fail-threshold 90}
+
+  :profiles {:dev {:plugins [[lein-ancient "0.6.15"]
+                             [lein-cloverage "1.0.11"]
+                             [lein-kibit "0.1.6"]
+                             [jonase/eastwood "0.2.5"]]
+
+                   :dependencies [[org.clojure/clojure "1.10.0"]]}})
