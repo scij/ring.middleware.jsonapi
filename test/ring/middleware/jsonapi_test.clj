ee68880d0139659183bf1b8442d23a01737aba77
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2019-01-24 14:01:53 +0100

    Add initial implementation #1

---
+++ test/ring/middleware/jsonapi_test.clj
@@ -0,0 +1,135 @@
+(ns ring.middleware.jsonapi-test
+  (:require [clojure.test :as t]
+            [jsonapi.core :as jsonapi]
+            [ring.middleware.jsonapi :as m-jsonapi]))
+
+(t/deftest wrap-response-test
+  (t/testing "should work with default options"
+    (t/are [expected response]
+        (= expected
+           (let [handler (m-jsonapi/wrap-response (constantly response))]
+             (handler {:headers {"accept" "application/vnd.api+json"}})))
+      {:status 200
+       :body {:data [{:id "1", :attributes {:name "foo"}, :type "bars"}]
+              :jsonapi {:version "1.0"}}}
+      {:status 200
+       :body [{:id "1", :name "foo"}]
+       ::jsonapi/id-key :id
+       ::jsonapi/resource-name "bars"}
+
+      {:status 200
+       :body {:data {:id "1", :attributes {:name "foo"}, :type "bars"},
+              :jsonapi {:version "1.0"}}}
+      {:status 200
+       :body {:id "1", :name "foo"}
+       ::jsonapi/id-key :id
+       ::jsonapi/resource-name "bars"}
+
+      {:status 200
+       :body {:data [{:id "1", :attributes {:name "foo"}, :type "bars"}]
+              :jsonapi {:version "1.0"}
+              :meta {:total 1}}}
+      {:status 200
+       :body [{:id "1", :name "foo"}]
+       ::jsonapi/id-key :id
+       ::jsonapi/resource-name "bars"
+       ::jsonapi/meta {:total 1}}))
+
+  (t/testing "should decorate response with a links object"
+    (t/is (= {:status 201
+              :body {:jsonapi {:version "1.0"}
+                     :data
+                     {:id "1"
+                      :type "bars"
+                      :attributes {:name "foo"}
+                      :links {:self "http://foo"}}}}
+             (let [handler (m-jsonapi/wrap-response
+                            (constantly {:status 201
+                                         :body {:id "1"
+                                                :name "foo"
+                                                ::jsonapi/links {:self "http://foo"}}
+                                         ::jsonapi/id-key :id
+                                         ::jsonapi/resource-name "bars"}))]
+               (handler {:headers {"accept" "application/vnd.api+json"}})))))
+
+  (t/testing "should not decorate response, error response code"
+    (t/is (= {:status 400
+              :body {:id "1", :name "foo"}}
+             (let [handler (m-jsonapi/wrap-response
+                            (constantly {:status 400
+                                         :body {:id "1", :name "foo"}}))]
+              (handler {:headers {"accept" "application/vnd.api+json"}})))))
+
+  (t/testing "should not decorate response, no accept header"
+    (t/is (= {:status 200
+              :body {:id "1", :name "foo"}}
+            (let [handler (m-jsonapi/wrap-response
+                            (constantly {:status 200
+                                         :body {:id "1", :name "foo"}}))]
+              (handler {})))))
+
+  (t/testing "should not decorate response, accept header doesn't match"
+    (t/is (= {:status 200, :body ""}
+            (let [handler (m-jsonapi/wrap-response
+                            (constantly {:status 200, :body ""}))]
+              (handler {:headers {"accept" "text/html"}})))))
+
+  (t/testing "should not decorate response, excluded URI"
+    (t/is (= {:status 200, :body ""}
+            (let [handler (m-jsonapi/wrap-response
+                           (constantly {:status 200, :body ""})
+                           {:excluded-uris #{"/login"}})]
+              (handler {:uri "/login"})))))
+
+  (t/testing "should not decorate response, no response code"
+    (t/is (= {:body ""}
+            (let [handler (m-jsonapi/wrap-response
+                            (constantly {:body ""}))]
+              (handler {:uri "/login"}))))))
+
+(t/deftest wrap-request-test
+  (t/testing "should work with default options"
+    (t/are [expected request]
+        (let [handler (m-jsonapi/wrap-request (fn [req]
+                                                (t/is (= expected req))))]
+          (handler request))
+      {:headers {"content-type" "application/vnd.api+json"}
+       :body {:name "foo"}}
+      {:headers {"content-type" "application/vnd.api+json"}
+       :body {:data {:attributes {:name "foo"}, :type "bars"}}}
+
+      {:headers {"content-type" "application/vnd.api+json"}
+       :body-params {:name "foo"}}
+      {:headers {"content-type" "application/vnd.api+json"}
+       :body-params {:data {:attributes {:name "foo"}, :type "bars"}}}))
+
+  (t/testing "should not work, no content type"
+    (let [request {:body {:data {:attributes {:name "foo"}, :type "bars"}}}
+          handler (m-jsonapi/wrap-request
+                   (fn [req]
+                     (t/is (= request req))))]
+      (handler request)))
+
+  (t/testing "should not work, excluded URI"
+    (let [request {:uri "/bars", :body {:data {:attributes {:name "foo"}, :type "bars"}}}
+          handler (m-jsonapi/wrap-request
+                   (fn [req]
+                     (t/is (= request req)))
+                   {:excluded-uris #{"/bars"}})]
+      (handler request)))
+
+  (t/testing "should not work, no content type"
+    (let [request {:body {:data {:attributes {:name "foo"}, :type "bars"}}}
+          handler (m-jsonapi/wrap-request
+                   (fn [req]
+                     (t/is (= request req))))]
+      (handler request)))
+
+  (t/testing "should not work, no content type regular expression"
+    (let [request {:headers {"content-type" "application/json"}
+                   :body {:data {:attributes {:name "foo"}, :type "bars"}}}
+          handler (m-jsonapi/wrap-request
+                   (fn [req]
+                     (t/is (= request req)))
+                   {:content-type-regexp nil})]
+      (handler request))))
